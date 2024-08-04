c0e371e086b0cb2f7111670a9559661a7ae217ba
tathagat2602 <153678532+tathagat2602@users.noreply.github.com>
2024-08-04 17:27:06 +0530

    Time: 0 ms (100%), Space: 8.3 MB (18.88%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revN = 0;
+        
+        while(x != 0){
+            int ld = x % 10;
+            // Check for overflow
+            if (revN > INT_MAX/10 || (revN == INT_MAX / 10 && ld > 7)) return 0;
+            if (revN < INT_MIN/10 || (revN == INT_MIN / 10 && ld < -8)) return 0;
+            revN = revN * 10 + ld;
+            x = x / 10;
+        }
+        
+        return revN;
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         int revN=0;
+        
+//         while(x > 0){
+//         int ld= x % 10;
+//         x = x /10;
+//         revN= (revN*10) + ld;
+//         }
+//         cout<<revN<<endl;
+//     };
+// };
