2a62e624c45db8775833ae88cff1e687e061bbb9
tathagat2602 <153678532+tathagat2602@users.noreply.github.com>
2024-09-06 19:08:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (24.83%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        // Check if lengths are the same, otherwise return false
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        // If the strings are already equal, return true
+        if (s == goal) {
+            return true;
+        }
+
+        int n = s.length();
+        
+        // Try all possible rotations using the three-reversal method
+        for (int k = 1; k < n; ++k) {
+            string temp = s;
+
+            // Reverse the entire string
+            reverse(temp.begin(), temp.end());
+
+            // Reverse the first k elements
+            reverse(temp.begin(), temp.begin() + k);
+
+            // Reverse the remaining elements
+            reverse(temp.begin() + k, temp.end());
+
+            // If this matches the goal, return true
+            if (temp == goal) {
+                return true;
+            }
+        }
+
+        // If no rotation matches, return false
+        return false;
+    }
+};
