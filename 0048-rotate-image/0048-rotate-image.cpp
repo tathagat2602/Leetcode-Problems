8a5c240316780cecceb01c0e7b34d0e4c7c46461
tathagat2602 <153678532+tathagat2602@users.noreply.github.com>
2025-07-21 10:44:28 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.48%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int rows= matrix.size();
-        int cols= matrix[0].size();
-
-        //transpose of a matrix
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<=i; j++){
-                swap(matrix[i][j], matrix[j][i]);
+        int n=matrix.size();
+        for(int i=0;i<n; i++){
+            for(int j=i+1; j <n; j++){
+                swap(matrix[i][j] , matrix[j][i]);
             }
         }
+        for(int i=0; i<n; i++){
 
-        //reverse each row to obtain the rotated (90 degree) image
-        for(int i=0; i<rows; i++){
-            reverse(matrix[i].begin(), matrix[i].end());
+        reverse(matrix[i].begin() , matrix[i].end());
         }
         
     }
