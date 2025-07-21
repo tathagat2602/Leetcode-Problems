66b03e87f41f96113f15a8c0e62789fd2eca3544
tathagat2602 <153678532+tathagat2602@users.noreply.github.com>
2025-07-21 13:38:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (38.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;     // Pointer for the last element in the m elements of nums1
-        int j = n - 1;     // Pointer for the last element in nums2
-        int k = m + n - 1; // Pointer for the last position in nums1
-
-        // Merge in reverse order
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
+        int i = m-1;
+        int j= n-1;
+        int index = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[index]=nums1[i];
+                index--;
                 i--;
-            } else {
-                nums1[k] = nums2[j];
+            }
+            else{
+                nums1[index]=nums2[j];
+                index--;
                 j--;
             }
-            k--;
         }
-
-        // If there are remaining elements in nums2
-        while (j >= 0) {
-            nums1[k] = nums2[j];
+        while(j>=0){
+            nums1[index]=nums2[j];
+            index--;
             j--;
-            k--;
         }
         
-        // No need to handle remaining elements in nums1 as they are already in place
     }
 };
